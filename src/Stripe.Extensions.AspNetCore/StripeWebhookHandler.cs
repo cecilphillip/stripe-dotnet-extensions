6989f48c024b1758ef0c852d6a330169c5e4d419
Cecil Phillip <cecilphillip@gmail.com>
2025-12-15 14:51:59 -0500

    clean up

--- src/Stripe.Extensions.AspNetCore/StripeWebhookHandler.cs
+++ src/Stripe.Extensions.AspNetCore/StripeWebhookHandler.cs
@@ -7,7 +7,7 @@ namespace Stripe.Extensions.AspNetCore;
 public abstract partial class StripeWebhookHandler<T>(StripeWebhookContext context)
 {
     protected StripeWebhookContext Context { get; } = context;
-    protected ILogger<T> Logger => context.LoggerFactory.CreateLogger<T>();
+    protected ILogger<T> Logger => Context.LoggerFactory.CreateLogger<T>();
     
     public async Task<IResult> ExecuteAsync()
     {
